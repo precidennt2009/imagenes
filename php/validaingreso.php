87cadd7b6caa17855918d48b0b510f11924e30ee
precidennt2009 <precidennt2009@hotmail.com>
2023-03-27 18:12:15 -0500

    second commit

--- php/validaingreso.php
+++ php/validaingreso.php
@@ -5,15 +5,22 @@ echo $nombre;
 $mysqli = new mysqli("127.0.0.1", "root", "robotica", "imagenes", 3306);
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    header ("Location: ../index.html");
 }
-/*VALIDAR INGRESO DEL USUARIO*/
-
-$result = $db->query("Call validar_usuario($_POST['username'], $_POST['password'])");
-while ($row = $result->fetch_assoc())
-{
-    var_dump($row);
+else{
+    /*VALIDAR INGRESO DEL USUARIO*/
+    $consulta="CALL VALIDAR_INGRESO(".$_POST['username'].", ".$_POST['password'].")";
+    echo $consulta;
+    $result=$mysqli->query($consulta);
+    $r = $result->fetch_array();
+    if($r["NUSUARIOS"]==1){
+        echo "entrando";
+    }
+    else{
+        header ("Location: ../index.html");
+    }
+    /*while ($r = $result->fetch_array()) {
+        echo $r["NUSUARIOS"] . "<br>";
+    }*/
 }
-
-header ("Location: ../index.html");
-
 ?>
